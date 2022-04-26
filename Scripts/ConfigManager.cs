917c9c41f9c83232f5c49fd4bedcb779e8015cdb
Radoslav Chobanov <rado.chobanov97@gmail.com>
2022-04-26 10:37:56 +0300

    First working bot

---
+++ Scripts/ConfigManager.cs
@@ -0,0 +1,57 @@
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace qguaratBot
+{
+    public static class ConfigManager
+    {
+        private static string ConfigFolder = "Resources";
+        private static string ConfigFile = "config.json";
+        private static string ConfigPath = ConfigFolder + "/" + ConfigFile;
+
+        public static BotConfig Config { get; private set; }
+
+        static ConfigManager()
+        {
+            if (!Directory.Exists(ConfigFolder))
+                Directory.CreateDirectory(ConfigFolder);
+
+            if (!File.Exists(ConfigPath))
+            {
+                Config = new BotConfig();
+                var json = JsonConvert.SerializeObject(Config, Formatting.Indented);
+                File.WriteAllText(ConfigPath, json);
+            }
+            else
+            {
+                var json = File.ReadAllText(ConfigPath);
+                Config = JsonConvert.DeserializeObject<BotConfig>(json);
+            }
+        }
+    }
+
+    public struct BotConfig
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        [JsonProperty("prefix")]
+        public string Prefix { get; private set; }
+
+        [JsonProperty("hostname")]
+        public string Hostname { get; private set; }
+
+        [JsonProperty("port")]
+        public int Port { get; private set; }
+
+        [JsonProperty("password")]
+        public string Password { get; private set; }
+
+        [JsonProperty("caseSensitive")]
+        public bool CaseSensitive { get; private set; }
+
+        // [JsonProperty("minimumLogLevel")]
+        // public LogLevel MinimumLogLevel { get; private set; }
+
+    }
+}
---
+++ Scripts/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace qguaratBot
+{
+    class Program
+    {
+        static void Main()
+        {
+            Bot bot = new Bot();
+            bot.MainSync().GetAwaiter().GetResult();
+        }
+    }
+}
